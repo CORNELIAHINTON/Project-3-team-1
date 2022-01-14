c0553b95853ac46a068264fea99e4cd6240627d9
CORNELIAHINTON <CORNELIAHINTONMILLER@GMAIL.COM>
2022-01-13 21:31:10 -0500

    renamed folder
    renamed folder and started app.py code

---
+++ Project 3 Team 1/app.py
@@ -0,0 +1,20 @@
+#Load libraries/import dependencies
+import musicmagic as mm
+from flask import Flask, jsonify, request, render_template
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+# Setup configuration for flask app
+app = Flask(__name__)
+
+# connect to database
+
+
+# Create routes
+#Web route
+
+#API route(s)
+
+
+# Run the application
